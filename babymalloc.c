72725b3e3c5f2e3c5b1b9cc7625b5830849e457f
Amr2812 <amr.osama.elmohamady@gmail.com>
2024-08-18 04:38:16 +0300

    feat: block coalescing + fix: first block footer set

--- babymalloc.c
+++ babymalloc.c
@@ -20,15 +20,18 @@
 #endif
 
 void *heap_startp;
+void *heap_endp;
 
 static void *extend_heap(size_t size);
 static void *find_fit(size_t size);
 static void insert_block(void *blkp, size_t size);
+static void coalesce(void *blkp);
 
 void *babymalloc(size_t size) {
     size = ALIGN(size);
     if (!heap_startp) {
         heap_startp = extend_heap(size);
+        heap_endp = heap_startp + size + 2 * WSIZE;
         debug_print("heap_startp: %p\n", heap_startp);
         debug_print("heap_startp value: %lu\n", *(uint64_t *) heap_startp);
         if (heap_startp == (void *) -1) {
@@ -47,6 +50,7 @@ void *babymalloc(size_t size) {
         return NULL;
     }
 
+    heap_endp = blkp + size + 2 * WSIZE;
     insert_block(blkp, size);
     return blkp + WSIZE;
 }
@@ -56,6 +60,7 @@ void babyfree(void *ptr) {
     SET_FREE(blkp);
     SET_FREE(blkp + GET_SIZE(blkp) + WSIZE);
     memset(ptr, 0, GET_SIZE(blkp));
+    coalesce(blkp);
 }
 
 static void *extend_heap(size_t size) {
@@ -65,7 +70,9 @@ static void *extend_heap(size_t size) {
     }
 
     *(uint64_t *) blkp = size;
+    *(uint64_t *) (blkp + size + WSIZE) = size;
     SET_FREE(blkp);
+    SET_FREE(blkp + size + WSIZE);
     return blkp;
 }
 
@@ -89,13 +96,68 @@ static void insert_block(void *blkp, size_t size) {
     SET_USED(blkp + size + WSIZE); // set footer
 }
 
+static void coalesce(void *blkp) {
+    uint64_t blk_size = GET_SIZE(blkp);
+    void *prev_blkp = NULL;
+    void *next_blkp = blkp + blk_size + 2 * WSIZE;;
+
+    int coalesce_prev = 0;
+    int coalesce_next = 0;
+
+    // check if the block is the first block
+    // if it is, we don't need to coalesce with the previous block
+    if (blkp == heap_startp) {
+        coalesce_prev = 0;
+    } else {
+        prev_blkp = blkp - (GET_SIZE(blkp - WSIZE) + 2 * WSIZE);
+        if (!GET_USED(prev_blkp)) {
+            coalesce_prev = 1;
+        }
+    }
+
+    // check if the block is the last block
+    // if it is, we don't need to coalesce with the next block
+    if (next_blkp >= heap_endp) {
+        coalesce_next = 0;
+    } else if (!GET_USED(next_blkp)) {
+        coalesce_next = 1;
+    }
+
+    if (coalesce_prev && coalesce_next) {
+        blk_size += GET_SIZE(prev_blkp) + GET_SIZE(next_blkp) + 4 * WSIZE;
+        void *next_blk_footer = next_blkp + GET_SIZE(next_blkp) + WSIZE;
+        *(uint64_t *) prev_blkp = blk_size;
+        *(uint64_t *) next_blk_footer = blk_size;
+        SET_FREE(prev_blkp);
+        SET_FREE(next_blk_footer);
+    } else if (coalesce_prev) {
+        blk_size += GET_SIZE(prev_blkp) + 2 * WSIZE;
+        *(uint64_t *) prev_blkp = blk_size;
+        void *blk_footer = blkp + blk_size + WSIZE;
+        *(uint64_t *) blk_footer = blk_size;
+        SET_FREE(prev_blkp);
+        SET_FREE(blk_footer);
+    } else if (coalesce_next) {
+        blk_size += GET_SIZE(next_blkp) + 2 * WSIZE;
+        void *next_blk_footer = next_blkp + GET_SIZE(next_blkp) + WSIZE;
+        *(uint64_t *) blkp = blk_size;
+        *(uint64_t *) next_blk_footer = blk_size;
+        SET_FREE(blkp);
+        SET_FREE(next_blk_footer);
+    }
+
+    if (prev_blkp == heap_startp) {
+        *(uint64_t *) heap_startp = blk_size;
+    }
+}
+
 void print_heap() {
     void *blkp = heap_startp;
     while (*(uint64_t *) blkp) {
         if (GET_USED(blkp)) {
-            printf("Block at %p: size %lu, used\n", blkp, GET_SIZE(blkp));
+            printf("Block at %p: size %lu %lu, used\n", blkp, GET_SIZE(blkp), GET_SIZE(blkp + GET_SIZE(blkp) + WSIZE));
         } else {
-            printf("Block at %p: size %lu, free\n", blkp, GET_SIZE(blkp));
+            printf("Block at %p: size %lu %lu, free\n", blkp, GET_SIZE(blkp), GET_SIZE(blkp + GET_SIZE(blkp) + WSIZE));
         }
         blkp += (GET_SIZE(blkp) + 2 * WSIZE);
     }
--- test.c
+++ test.c
@@ -6,8 +6,9 @@
 
 #include "babymalloc.h"
 
-void testFreeThenMallocAlignment() {
+void test_free_then_malloc_alignment() {
     void *ptr = babymalloc(10);
+    print_heap();
     void *ptr2 = babymalloc(10);
     babyfree(ptr);
     void *ptr3 = babymalloc(16);
@@ -15,8 +16,43 @@ void testFreeThenMallocAlignment() {
     assert(ptr == ptr3);
 }
 
+void test_prev_block_coalescing() {
+    void *ptr = babymalloc(16);
+    void *ptr2 = babymalloc(16);
+    babyfree(ptr);
+    babyfree(ptr2);
+    void *ptr3 = babymalloc(32);
+
+    assert(ptr == ptr3);
+}
+
+void test_next_block_coalescing() {
+    void *ptr = babymalloc(16);
+    void *ptr2 = babymalloc(16);
+    babyfree(ptr2);
+    babyfree(ptr);
+    void *ptr3 = babymalloc(32);
+
+    assert(ptr == ptr3);
+}
+
+void test_next_and_prev_block_coalescing() {
+    void *ptr = babymalloc(16);
+    void *ptr2 = babymalloc(16);
+    void *ptr3 = babymalloc(16);
+    babyfree(ptr);
+    babyfree(ptr3);
+    babyfree(ptr2);
+    void *ptr4 = babymalloc(48);
+
+    assert(ptr == ptr4);
+}
+
 int main(int argc, char **argv) {
-    testFreeThenMallocAlignment();
+//    test_free_then_malloc_alignment();
+//    test_prev_block_coalescing();
+//    test_next_block_coalescing();
+    test_next_and_prev_block_coalescing();
 
     return 0;
 }
